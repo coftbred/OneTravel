c3760130c034984a7adb23d738ea48e23f893d20
Minh Quang <quangdo2000@gmail.com>
2021-06-09 16:58:22 +0700

    basic file for blog

---
+++ create.php
@@ -0,0 +1,11 @@
+<?php 
+    include 'includes/header.php';
+?>
+
+    <div class="contaier">
+        <form action="" method="post"></form>
+    </div>
+
+<?php 
+    include 'includes/footer.php'
+?>
